2794fcc18b051b71b80a3529b5db242e4a691fa5
Eric Batiste <ericbatiste4@gmail.com>
2025-01-08 11:50:36 -0700

    Initial Commit

---
+++ maptivity/maptivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  maptivityApp.swift
+//  maptivity
+//
+//  Created by Eric Batiste on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct maptivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ maptivityTests/maptivityTests.swift
@@ -0,0 +1,17 @@
+//
+//  maptivityTests.swift
+//  maptivityTests
+//
+//  Created by Eric Batiste on 1/8/25.
+//
+
+import Testing
+@testable import maptivity
+
+struct maptivityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
